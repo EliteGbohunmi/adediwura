f2334793ec0617732e27bd7d76fc2f4af36a9139
Gbohunmi Williams <elitegbohunmi@gmail.com>
2025-02-17 14:56:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function toggleMenu() {
   menuIcon.classList.toggle("active");
   document.querySelector("ul").classList.toggle("yh");
 }
+ScrollReveal().reveal(".gg", {
+  delay: 50,
+  origin: "left",
+  distance: "100px",
+});
